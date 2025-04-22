b466c1090ed13e0f4aa1c5be759090215d0723e9
Diego <dvillegas@cellit.cl>
2025-04-21 20:45:07 -0400

    Estructura inicial proyecto

---
+++ apiuser/src/test/java/com/sermaluc/prueba/apiuser/ApiuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sermaluc.prueba.apiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
